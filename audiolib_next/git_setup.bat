f9dc5c44e7c6a7dfb5e3cff1d9bf24a4d78fcea1
RiteshF7 <riteshf7@gmail.com>
2026-03-22 12:27:27 +0530

    Restructured repository: separated frontend and backend into their own directories

--- audiolib_next/git_setup.bat
+++
@@ -1,7 +0,0 @@
-git init
-git add .
-git commit -m "Initial commit Backend TTS & NextJS UI"
-git branch -M main
-git remote remove origin
-git remote add origin https://github.com/priyanshurao369-max/voice.git
-git push -u origin main -f 
